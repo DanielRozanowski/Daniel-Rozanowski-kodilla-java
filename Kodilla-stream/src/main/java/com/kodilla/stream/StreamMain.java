5d3409bb1d68fdc1a5d4ea1128c07308dda7f943
DanielRozanowski <81776490+DanielRozanowski@users.noreply.github.com>
2021-12-21 18:28:46 +0100

    7

--- Kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ Kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -5,7 +5,7 @@ import com.kodilla.stream.lambda.SaySomething;
 public class StreamMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 – Stream");
+        System.out.println("Welcome to module 7 - Stream");
 
         SaySomething saySomething = new SaySomething();
         saySomething.say();
