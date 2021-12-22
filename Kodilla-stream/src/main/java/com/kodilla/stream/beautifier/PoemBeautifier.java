7d71dacf82aa2e537c5d2b35654d277abacff806
DanielRozanowski <81776490+DanielRozanowski@users.noreply.github.com>
2021-12-22 15:32:08 +0100

    7.1.1.1

--- Kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ Kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -3,6 +3,6 @@ package com.kodilla.stream.beautifier;
 public class PoemBeautifier {
    public void poetBeautifier(String a, PoeamDecorator poeamDecorator) {
        String result = poeamDecorator.decorate(a);
-       System.out.println("Result" + result + "ResultEnd");
+       System.out.println("Result" + result);
    }
 }
