00a1599c56bbd5d637738885415d7ff4a2a28ccc
DanielRozanowski <81776490+DanielRozanowski@users.noreply.github.com>
2022-04-20 21:55:43 +0200

    13.2.2

--- kodilla-good-patterns/src/main/java/PatternsMain.java
+++ kodilla-good-patterns/src/main/java/PatternsMain.java
@@ -7,5 +7,10 @@ class Application {
         OrderProcessor orderProcessor = new OrderProcessor(new ConfirmService(),
                 new ProductOrderService(), new ProductOrderRepo());
         orderProcessor.process(orderRequest);
+            MovieStore movieStore = new MovieStore();
+
+            movieStore.getMovies().values().stream()
+                    .flatMap(strings -> strings.stream())
+                    .forEach(s -> System.out.print(s+"! "));
+        }
     }
-}
